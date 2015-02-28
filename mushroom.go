7caa738e41d4a6a2b23b521dba61d61e5acd227f
Alexander Bartels <bartels.xander@gmail.com>
2015-02-28 23:36:28 +0100

    connecting groupcache peers with doozerd

--- mushroom.go
+++ mushroom.go
@@ -9,8 +9,12 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"sync/atomic"
 	"github.com/gorilla/mux"
 	"github.com/golang/groupcache"
+	"github.com/ha/doozer"
 	"html/template"
 	"github.com/disintegration/imaging"
 )
@@ -19,6 +23,7 @@ import (
 var (
 	cacheAddr     = flag.String("cache", "127.0.0.1:8000", "Address for groupcache")
 	listenAddr    = flag.String("listen", "localhost:4000", "Address to listen on")
+	doozerAddr    = flag.String("doozer", "127.0.0.1:8046", "Doozerd Config Server")
 	imageSrc      = flag.String("imageSrc", "img/", "Directory with images to serve")
 	imgHandlePath = "/images/{imgFile}"
 
@@ -42,6 +47,13 @@ var (
 func main() {
 	// parse the command line flags
 	flag.Parse()
+
+	// Setup the doozer connection.
+	d, err := doozer.Dial(*doozerAddr)
+	if err != nil {
+		log.Fatalf("connecting to doozer: %v\n", err)
+	}
+	defer d.Close()
 
 	// Setup the cache.
 	pool = groupcache.NewHTTPPool("http://" + *cacheAddr)
@@ -68,17 +80,104 @@ func main() {
 	 *
 	 */
 
+	// Start watching for changes and signals.
+	go watch(d)
+
 	// setup the router
 	router := mux.NewRouter()
 	router.HandleFunc("/", rootHandler)
 	router.HandleFunc(imgHandlePath, imgHandler)
 
 	http.Handle("/", router)
-	err := http.ListenAndServe(*listenAddr, nil)
-	if err != nil {
-		log.Fatal(err)
+	httpErr := http.ListenAndServe(*listenAddr, nil)
+	if httpErr != nil {
+		log.Fatal(httpErr)
 	}
 
+}
+
+// watch updates the peer list of servers based on changes to the
+// doozer configuration or signals from the OS.
+func watch(d *doozer.Conn) {
+	peerFile := "/peers"
+	var peers []string
+	var rev int64
+
+	// Run the initial get.
+	data, rev, err := d.Get(peerFile, nil)
+	if err != nil {
+		log.Println("initial peer list get:", err)
+		log.Println("using empty set to start")
+		peers = []string{}
+	} else {
+		peers = strings.Split(string(data), " ")
+	}
+
+	// Add myself to the list.
+	peers = append(peers, "http://" + *cacheAddr)
+	rev, err = d.Set(peerFile, rev,
+		[]byte(strings.Join(peers, " ")))
+	if err != nil {
+		log.Println("unable to add myself to the peer list (no longer watching).")
+		return
+	}
+	pool.Set(peers...)
+	log.Println("added myself to the peer list.")
+
+	// Setup signal handling to deal with ^C and others.
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, os.Interrupt, os.Kill)
+
+	// Get the channel that's listening for changes.
+	updates := wait(d, peerFile, &rev)
+
+	for {
+		select {
+		case <- sigs:
+			// Remove ourselves from the peer list and exit.
+			for i, peer := range peers {
+				if peer == "http://" + *cacheAddr {
+					peers = append(peers[:i], peers[i+1:]...)
+					d.Set(peerFile, rev, []byte(strings.Join(peers, " ")))
+					log.Println("removed myself from peer list before exiting.")
+				}
+			}
+			os.Exit(1)
+		case update, ok := <-updates:
+			// If the channel was closed, we should stop selecting on it.
+			if !ok {
+				updates = nil
+				continue
+			}
+
+			// Otherwise, update the peer list.
+			peers = update
+			log.Println("got new peer list:", strings.Join(peers, " "))
+			pool.Set(peers...)
+		}
+	}
+}
+
+func wait(d *doozer.Conn, file string, rev *int64) chan []string {
+	c := make(chan []string, 1)
+	cur := *rev
+	go func() {
+		for {
+			// Wait for the change.
+			e, err := d.Wait(file, cur+1)
+			if err != nil {
+				log.Println("waiting failed (no longer watching):", err)
+				close(c)
+				return
+			}
+			// Update the revision and send the change on the channel.
+			atomic.CompareAndSwapInt64(rev, cur, e.Rev)
+			cur = e.Rev
+			c <- strings.Split(string(e.Body), " ")
+		}
+	}()
+
+	return c
 }
 
 func imgHandler(w http.ResponseWriter, r *http.Request) {
